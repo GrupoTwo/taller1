65447d44dbfdba12586b3e50ac2fe0102856beb4
DavidDurango <daviick19@hotmail.com>
2020-06-10 18:26:10 -0500

    Pasando pseudocodigo a java

---
+++ Triangulotaller/src/TallerIS.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class TallerIS {
+	
+	public static String TriangleType(int a, int b, int c) {
+		Boolean IsAtriangle;
+		
+		if((a<b+c) && (b<a+c) && (c<a+b)) {
+			IsAtriangle = true;
+		}
+		else {IsAtriangle = false;}
+		
+		if(IsAtriangle){
+		
+			if (a==b && b==c ) {
+				return "Equilateral";
+			}
+			else if(a!=b && a!=c && b!=c) {
+				return "Scalene";
+			}
+			else {
+				return "Isoceles";
+			}
+		}
+		
+		else {
+			return "Not a Triangle";
+		}
+
+	}
+ 
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		int error = 1;
+		while(error<2){
+		 System.out.println("Ingrese un número: ");
+		 a = s.nextInt();
+		 if(a>=1 && a<=200){
+			 System.out.println("Ingrese un número: ");
+			 b = s.nextInt();
+			 if(b>=1 && b<=200){
+				 System.out.println("Ingrese un número: ");
+				 c = s.nextInt();
+				 if(c>=1 && c<=200){
+					 error=error+1;
+				 }
+				 else {
+					 System.out.println("Value of c is not in the range of permitted values");
+				 }
+			 }
+			 else {
+				 System.out.println("Value of b is not in the range of permitted values");
+			 }
+		 }
+		 else {
+			 System.out.println("Value of a is not in the range of permitted values");
+		 }
+		 
+		 
+		}
+		System.out.println("Side A is " + a);
+		System.out.println("Side B is " + b);
+		System.out.println("Side C is " + c);
+		
+		
+		//-------------------------------------------------------------------------------------
+		//Casos de Prueba
+		
+		System.out.println("Test 1: "+ "Equilateral".equals(TriangleType(3,3,3)));
+		
+	}
+	
+}
+
+
+
